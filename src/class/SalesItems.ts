2890194e044f4355f20a9f0ee950173f19d215f5
Kim Jaedeok <actinium@ant-inc.co>
2024-09-22 15:22:26 +0900

    feat: add SalesItems class
    판매 물품을 관리할 수 있는 SalesItems class를 추가합니다.

---
+++ src/class/SalesItems.ts
@@ -0,0 +1,48 @@
+import { Product } from "../types/product";
+import autoBind from "auto-bind";
+
+export class SalesItems {
+  #items: Product[];
+  #itemStock = new Map<Product["name"], number>();
+
+  constructor(items: (Product & { stock: number })[]) {
+    autoBind(this);
+    this.#items = items.map(({ name, price }) => ({ name, price }) as Product);
+    items.forEach(({ name, stock }) => this.#itemStock.set(name, stock));
+  }
+
+  get list() {
+    return this.#items.map((item) => ({
+      name: item.name,
+      price: `${item.price.value}${item.price.currency}`,
+      stock: this.#getStockOf(item.name),
+    }));
+  }
+
+  #getStockOf(product: Product["name"]) {
+    return this.#itemStock.get(product) || 0;
+  }
+  hasStockOf(product: Product["name"]) {
+    return this.#getStockOf(product) > 0;
+  }
+  addItem(product: Product["name"], quantity: number) {
+    if (quantity < 0) {
+      throw new Error("수량은 0보다 작을 수 없습니다.");
+    }
+
+    this.#itemStock.set(product, this.#getStockOf(product) + quantity);
+  }
+  subtractItem(product: Product["name"], quantity: number) {
+    if (this.#itemStock.has(product)) {
+      if (quantity < 0) {
+        throw new Error("수량은 0보다 작을 수 없습니다.");
+      }
+
+      if (this.#getStockOf(product) < quantity) {
+        throw new Error("재고가 부족합니다.");
+      }
+
+      this.#itemStock.set(product, this.#getStockOf(product) - quantity);
+    }
+  }
+}
--- src/class/VendingMachine.ts
+++ src/class/VendingMachine.ts
@@ -1,19 +1,41 @@
+import { Product } from "../types/product";
 import { VendingMachine as IVendingMachine } from "../types/vendingMachine";
 import { ChangeIndicator } from "./ChangeIndicator";
+import { SalesItems } from "./SalesItems";
 import autoBind from "auto-bind";
 
 export interface VendingMachineParams {
+  salesItems: SalesItems;
   changeIndicator: ChangeIndicator;
 }
 export class VendingMachine implements IVendingMachine {
+  #salesItems;
   #changeIndicator;
 
-  constructor({ changeIndicator }: VendingMachineParams) {
+  constructor({ salesItems, changeIndicator }: VendingMachineParams) {
     autoBind(this);
+    this.#salesItems = salesItems;
     this.#changeIndicator = changeIndicator;
   }
 
+  get salesItems() {
+    return this.#salesItems.list.map((item) => ({
+      name: item.name,
+      price: item.price.toString(),
+      sellable: this.#checkSellable(item.name),
+    }));
+  }
   get changeValue() {
     return this.#changeIndicator.toString();
+  }
+
+  #checkSellable(product: Product["name"]) {
+    if (!this.#salesItems.hasStockOf(product)) {
+      return false;
+    }
+
+    //TODO - 보충 필요
+
+    return true;
   }
 }
